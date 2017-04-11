4dc996d630301316f8e5e1935418473925b9b9c3
Christian Dersch <lupinix@mailbox.org>
2017-04-11 22:48:11 +0200

    split tables for NGC and IC

--- create_ngc_fits_table.py
+++ create_ngc_fits_table.py
@@ -12,15 +12,18 @@ from astropy.io import ascii, fits
 from astropy.table import Table
 import astropy.units as u
 
-openngc_csv = "/home/lupinix/OpenNGC/NGC.csv"
+openngc_csv = "OpenNGC/NGC.csv"
 
 ngcic = ascii.read(openngc_csv,delimiter=";")
 table = Table([ngcic["Name"],
-    Angle(ngcic["RA"],unit="hourangle").degree,
-    Angle(ngcic["Dec"],unit=u.deg).degree,
+    Angle(ngcic["RA"],unit="hourangle").to(u.deg),
+    Angle(ngcic["Dec"],unit=u.deg),
     ngcic["Type"],
     (ngcic["Smax"]*u.arcmin/2).to(u.deg)],
     names=("Name","RA","Dec","Type","Radius"))
 table.sort("Dec")
-#table_ic = table[(table["Name"])[0]=="I"]
-print(table[table["Name"]=="IC1525"])
+
+table_ngc = table[np.char.startswith(table["Name"],"NGC")]
+table_ic = table[np.char.startswith(table["Name"],"IC")]
+
+print(table_ngc)
